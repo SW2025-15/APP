f36f38a08713f4b857d9188924350b1438f44178
Konishi <2511141003x@ed.fuk.kindai.ac.jp>
2025-11-26 08:56:16 +0000

    本日の作業終了。タイトル、画像、本文を含めて投稿できるようにしている途中

--- myapp/app/controllers/posts_controller.rb
+++ myapp/app/controllers/posts_controller.rb
@@ -1,4 +1,33 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      # 保存成功でshow画面へ
+      redirect_to @post, notice: "投稿が完了しました。"
+    else
+      # 保存失敗で、new画面を再表示+エラーメッセージ
+      render :new, status: :unprocessable_entity 
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body, :status, images: [])
+  end
+  
+  
 end
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  resources :posts
+  
   root 'posts#index'
   get "posts/index"
   get 'posts/new', to: 'posts#new'
+  
+  post 'bookmarks', to: 'bookmarks#create'
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
