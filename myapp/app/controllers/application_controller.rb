fcd4b7f5532cba19d41ef17bbf38fe6f5d704e4e
Konishi <2511141003x@ed.fuk.kindai.ac.jp>
2025-12-17 09:01:02 +0000

    本日の作業終了。ログインログアウト機能の実装

--- myapp/app/controllers/application_controller.rb
+++ myapp/app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # サインアップ（新規登録）とログイン時に student_id を許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:student_id])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:student_id])
+  end
 end
--- myapp/app/models/user.rb
+++ myapp/app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
-    
+  # Devise設定
+  devise :database_authenticatable, :registerable,
+        :rememberable, :validatable
+  
+  # 学籍番号は10桁
+  validates :student_id, presence: true, uniqueness: true, length: { is: 10 }, format: { with: /\A\d+\z/ }
+
+  # メールアドレスを不要にする
+  #「保存する前に、学籍番号@example.com というメールアドレスを自動セットする」
+  before_validation :set_dummy_email
+
+  private
+  def set_dummy_email
+    self.email = "#{student_id}@example.com" if student_id.present?
+  end
 end
